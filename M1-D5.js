92aaf96db6656d9bd14090a28904297d2fd1881d
BertoPolo <bertopolo91@gmail.com>
2021-12-03 16:58:57 +0100

    Update M1-D5.js

--- M1-D5.js
+++ M1-D5.js
@@ -50,35 +50,60 @@ console.log(crazyDiff(66))
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
-const boundary= function(number){
+const boundary = function(n){
+    if(n>=20 && n<=100 || n==400){
+        return true
+    }else{
+        return false
+    }
 
 }
+
 
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const strivify = function(strPar){
+    if( strPar.startsWith("Strive")){
+        return strPar
+    }else{
+        return "Strive" + strPar
+    }   
+}
 
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
  HINT: Modulus Operator
 */
 
-/* WRITE YOUR ANSWER HERE */
+const check3and7 = function(posNum){
+    if (posNum>0 && (posNum % 3 == 0) || (posNum % 7 == 0 )){
+        return true
+    }else{ 
+        return  false
+    }
+}
 
 /* EXERCISE 7
  Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR ANSWER HERE */
+const reverseString = function(strToReverse){
+    reverseString(strToReverse)
+}
 
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const upperFirst = function(capLetters){
+    for (let i=0;i<capLetters.length;i++){
+        
+    }
+
+}
 
 /* EXERCISE 9
  Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
